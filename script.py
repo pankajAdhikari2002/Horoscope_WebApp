e735e06c538f816b3440ff9db61d3a532bb8ecfd
Pankaj Adhikari <104145548+pankajAdhikari2002@users.noreply.github.com>
2024-05-15 19:45:45 +0530

    Update script.py
    Error fixed, and extraction of data enhanced!

--- script.py
+++ script.py
@@ -8,9 +8,7 @@ def runScript():
     try:
         today = datetime.date.today()
         day, month, year = today.strftime("%d %B %Y").split(" ")
-
         url = f"https://www.vogue.in/horoscope/collection/horoscope-today-{month.lower()}-{int(day)}-{year}/"
-
         response = requests.get(url)
         response.raise_for_status()  # Raise an error for bad response status codes
 
@@ -24,23 +22,24 @@ def runScript():
 
         data_f = {}
         data_f["Date"] = f"{day} {month} {year}"
+
         # Loop through the found elements
         for element in elements_with_class:
             symbol = element.h2.text.split(" ")[0]
-            description, cosmic_tip = element.find_all('p')
-
-            data_f[symbol] = {"desc": description.text, "cosmic_tip": cosmic_tip.text.split(":")[1].strip()}
+            content = element.find(class_="product-summary").text
+            desc, cosmic_tip = content.split("Cosmic tip: ")
+            data_f[symbol] = {"desc": desc, "cosmic_tip": cosmic_tip}
 
         # Serializing json
         json_object = json.dumps(data_f, indent=4, ensure_ascii=False)
-    
+
         # Writing to sample.json
         with open(file="horoscope.json", mode="w", encoding="utf-8") as outfile:
             outfile.write(json_object)
         return {"Message": "Script ran successfully!"}
 
     except requests.exceptions.RequestException as e:
-        return {"Request-Error" : str(e)}
+        return {"Message for Request Error" : str(e)}
 
     except Exception as e:
-        return {"Script-Error" : str(e)}
+        return {"Message for Script Error" : str(e)}
